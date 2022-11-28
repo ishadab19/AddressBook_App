3d115bb400e57aa21cb1ce17504eb82b160ec247
shadab <me.shadab19@gmail.com>
2022-11-28 19:06:54 +0530

    [Added - Form validation ex - mobile number , address, first name ]

--- addressBook.js
+++ addressBook.js
@@ -13,34 +13,10 @@ function hello(event) {
         alert("Enter valid name");
         document.getElementById("name-err").innerHTML = "Min 3 Char & First Letter Should Be Capital"
     }
-
-    // For Address Regex 
-    event.preventDefault();
-    let addressRegex = /^[A-Z]{1}[a-z]{5,6}(- _ .)[A-Z]$^/;
-    let address = document.getElementById('address').value;
-    if (addressRegex.test(address)) {
-        localStorage.setItem("address", name);
-        alert("Hello, " + localStorage.getItem("address"));
-    } else {
-        alert("Enter Valid Address");
-        document.getElementById('address-err').innerHTML = "Min  6 Chars & First Letter  Should Be capital"
-    }
-
-    // For Number regex 
-    event.preventDefault();
-    let Number = /(91)()[6-9]{1}[0-9]{9}$^/;
-    let number = document.getElementById('number').value;
-    if (Number.test(number)) {
-        localStorage.setItem("number", number);
-        alert("Hello, " + localStorage.getItem("number"));
-    } else {
-        alert("Enter Valid number");
-        document.getElementById('number-err').innerHTML = " country code  & first Number 6 - 9 "
-    }
 }
 
 function check(event)
-{   //For Name Regex
+{    //form First Name
     event.preventDefault();
     let firstNameRegex = /^[A-Z]{1}[a-z]{2,}$/;
     let name = document.getElementById('name').value;
@@ -54,7 +30,22 @@ document.getElementById('form')
    .addEventListener('submit',hello);
 
 
-   function check(event)
+   function hello1(event){
+    // For Address Regex 
+    event.preventDefault();
+    let addressRegex = /^[A-Z]{1}[a-z]{5,6}(- _ .)[A-Z]$^/;
+    let address = document.getElementById('address').value;
+    if (addressRegex.test(address)) {
+        localStorage.setItem("address", address);
+        alert("Hello, " + localStorage.getItem("address"));
+    } else {
+        alert("Enter Valid Address");
+        document.getElementById('address-err').innerHTML = "Min  6 Chars & First Letter  Should Be capital"
+    }
+
+   }
+
+   function check1(event)
    {    // For Address Regex
        event.preventDefault();
        let addressRegex = /^[A-Z]{1}[a-z]{5,6}(- _ .)[A-Z]$^/;
@@ -66,22 +57,35 @@ document.getElementById('form')
       document.getElementById("addresss-err").innerHTML =""
    }
    document.getElementById('form')
-      .addEventListener('submit',hello);
+      .addEventListener('submit',hello1);
 
+function hello2(event)
+{
+     //Mobile Number regex 
+     event.preventDefault();
+     let Number = /(91)()[6-9]{1}[0-9]{9}$^/;
+     let number = document.getElementById('number').value;
+     if (Number.test(number)) {
+         localStorage.setItem("number", number);
+         alert("Hello, " + localStorage.getItem("number"));
+     } else {
+         alert("Enter Valid number");
+         document.getElementById('number-err').innerHTML = " country code  & first Number 6 - 9 "
+     }
+ }
 
-      function check(event)
-   {    //Mobile Number Regex
-    event.preventDefault();
-    let Number = /(91)()[6-9]{1}[0-9]{9}$^/;
-   let number = document.getElementById('number').value;
-       if(!Number.test(number))
-       document.getElementById("number-err").innerHTML ="Min 10 Digit Required"
-      else
-      document.getElementById("number-err").innerHTML =""
-   }
-   document.getElementById('form')
-      .addEventListener('submit',hello);
-
+ function check2(event)
+ {    //Mobile Number Regex
+  event.preventDefault();
+  let Number = /(91)()[6-9]{1}[0-9]{9}$^/;
+ let number = document.getElementById('number').value;
+     if(!Number.test(number))
+     document.getElementById("number-err").innerHTML ="Min 10 Digit Required"
+    else
+    document.getElementById("number-err").innerHTML =""
+ }
+ document.getElementById('form')
+    .addEventListener('submit',hello2);
 
 
 
